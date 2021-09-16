94f657035599575665c3b8bcedeb78856d753f96
likelian <74072319+likelian@users.noreply.github.com>
2021-09-16 19:20:43 -0400

    Delete Lab_fall2021 directory

--- Lab_fall2021/Source/EmptyTFRecord.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-#import numpy as np
-#import IPython.display as display
-
-
-filename = 'test.tfrecord'
-writer = tf.data.experimental.TFRecordWriter(filename)
-writer.write()
