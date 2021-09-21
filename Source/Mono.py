02a9bfea465dde1d83b0670443a570c1b3b47c06
likelian <likelian@me.com>
2021-09-21 18:44:54 -0400

    essentia for shortTermLUFS

--- Source/Mono.py
+++ Source/Mono.py
@@ -1,6 +1,9 @@
 import soundfile as sf
 import numpy as np
-import pyloudnorm as pyln
+import sys
+sys.path.append('/usr/local/lib/python3.8/site-packages')
+import essentia
+import essentia.standard as ess
 import json
 
 
@@ -9,24 +12,43 @@ filename = "amy_1"
 wav = ".wav"
 dir_filename = dir + filename + wav
 
-data, samplerate = sf.read(dir_filename)
-#length = data.shape[0] / samplerate
+data, sampleRate = sf.read(dir_filename)
 
-acc = (data.T[0]/2).T #Accompaniment
-vox = (data.T[1]/2).T #Vocal
+acc = np.array([data.T[0]/2, data.T[0]/2], dtype=np.float32).T #Accompaniment
+vox = np.array([data.T[1]/2, data.T[1]/2], dtype=np.float32).T #Vocal
 
 
-meter = pyln.Meter(samplerate) # create BS.1770 meter
+########################################################
 
-acc_integratedLUFS = meter.integrated_loudness(acc)
-vox_integratedLUFS = meter.integrated_loudness(vox)
+"""
+from essentia.standard import *
+w = Windowing(type = 'hann')
+spectrum = Spectrum()  # FFT() would return the complex FFT, here we just want the magnitude spectrum
+mfcc = MFCC()
+"""
 
-integratedLUFS = {"acc_integratedLUFS": acc_integratedLUFS,
-                  "vox_integratedLUFS": vox_integratedLUFS}
+def shortTermLoudness(buffer, SR=44100, HS=0.1):
+    """
+    default sample rate: 44100Hz
+    default hop size: 0.1s
+
+    return: an array of shortTermLoudness in dB
+    """
+    LoudnessEBUR128 = essentia.standard.LoudnessEBUR128(sampleRate=SR, hopSize=HS)
+    shortTermLoudness = LoudnessEBUR128(buffer)[1]
+    return shortTermLoudness
+
+
+acc_shortTermLoudness = shortTermLoudness(acc, SR=sampleRate)
+vox_shortTermLoudness = shortTermLoudness(vox, SR=sampleRate)
+
+
+########################################################
+
 
 ground_truth = {}
-ground_truth[filename] = integratedLUFS
-
+ground_truth[filename+"_acc_shortTermLoudness"] = acc_shortTermLoudness.tolist()
+ground_truth[filename+"_vox_shortTermLoudness"] = vox_shortTermLoudness.tolist()
 
 
 ground_truth_dir = "../JSON/ground_truth/"
@@ -34,8 +56,7 @@ with open(ground_truth_dir + 'ground_truth.txt', 'w') as outfile:
     json.dump(ground_truth, outfile)
 
 
+mixture_Data = (acc.T + vox.T).T #mono_sum
 
-mono_Data = (acc.T + vox.T).T #mono_sum
-
-mono_dir_filename = dir + "mono_" + filename + wav
-sf.write(mono_dir_filename, mono_Data, samplerate)
+mixture_dir_filename = dir + "mixture_" + filename + wav
+sf.write(mixture_dir_filename, mixture_Data, sampleRate)
